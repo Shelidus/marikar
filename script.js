a48113c0b5ea006bbe0e5bb00306abafefbd7bce
Shelidus <114400197+Shelidus@users.noreply.github.com>
2024-08-24 16:34:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('applyJob').addEventListener('click', function() {
+    window.open('https://in.indeed.com/cmp/Marikar-(motors)-Ltd-1/jobs', '_blank');
+});
+
+document.getElementById('leaveJob').addEventListener('click', function() {
+    window.open('https://www.resumemaker.online/free-tools/resignation-letter-generator-free', '_blank');
+});
